3cb2f3b325a474626ed919f2361e7817832da0f0
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2024-07-17 01:47:09 +0900

    [chore] set env CICD

--- src/main/java/space/space_spring/Controller/TestController.java
+++ src/main/java/space/space_spring/Controller/TestController.java
@@ -1,21 +1,42 @@
 package space.space_spring.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import space.space_spring.argument_resolver.JwtPreAuth;
+import space.space_spring.config.testConfig;
 import space.space_spring.response.BaseResponse;
 
 @RestController
 @Slf4j
 public class TestController {
+    private testConfig testEnv;
+    @Autowired
+    public TestController(testConfig testEnv) {
+        this.testEnv = testEnv;
+    }
+    @Value("${secret.jwt-secret-key}")
+    private String JWT_SECRET_KEY;
+    @Value("${secret.jwt-expired-in}")
+    private Long JWT_EXPIRED_IN;
 
+
+    @Value("${spring.datasource.url}")
+    private String DB_URL;
 
     @GetMapping("/testEnv")
     public String testEndpoint() {
-        return "Test Environment Value: " ;
+        return "Test Environment Value: "+testEnv.getEnv() ;
     }
 
+    @GetMapping("/testEnvSec")
+    public String testSecretENVEndpoint() {
+        return "Test <br>JWT_SECRET_KEY: "+JWT_SECRET_KEY
+                +"<br>JWT_EXPIRED_IN: "+JWT_EXPIRED_IN+
+                "<br>DB URL: "+DB_URL;
+    }
     @GetMapping("/test")
     public String test(){
         return "CI/CD 환경 구축 테스트 중. 이 메세지가 보인다면 성공입니다";
