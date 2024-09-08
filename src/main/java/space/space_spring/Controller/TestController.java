ac58240cb7e08fc21414dd58ff0a8b50b7330a73
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2024-09-08 17:42:33 +0900

    [test] flow test 2

--- src/main/java/space/space_spring/Controller/TestController.java
+++ src/main/java/space/space_spring/Controller/TestController.java
@@ -51,6 +51,6 @@ public class TestController {
     @GetMapping("/test/chore")
     public BaseResponse<String> choreBranchTest(@JwtPreAuth Long userId) {
         log.info("userId = {}", userId);
-        return new BaseResponse<>("이 글자가 보인다면, chore branch 가 배포 된 것입니다. 테스트 중입니다.");
+        return new BaseResponse<>("이 글자가 보인다면, chore branch 가 배포 된 것입니다. 테스트 중입니다. 다시 테스트 중입니다");
     }
 }
